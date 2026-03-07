a7f1ed99d253be9b036ca2047d8bce882bfe4ea8
gabe-pud <gabrielgarciasantana3@gmail.com>
2026-03-07 18:46:19 -0300

    Initial commit

---
+++ src/main/java/br/edu/fatecpg/deolhonolixo/DeOlhoNoLixoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.deolhonolixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeOlhoNoLixoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeOlhoNoLixoApplication.class, args);
+	}
+
+}
