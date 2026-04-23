81a3f674fd3b69b2b25ba5cf2bc56fb56a0982a7
gabe-pud <gabrielgarciasantana3@gmail.com>
2026-04-22 22:36:47 -0300

    feat(config): setup MongoDB client, enabled auto-index creation, updated incorrect dependency in pom.xml and removed unnecessary autoconfiguration exclusion

--- src/main/java/br/edu/fatecpg/deolhonolixo/DeOlhoNoLixoApplication.java
+++ src/main/java/br/edu/fatecpg/deolhonolixo/DeOlhoNoLixoApplication.java
@@ -1,12 +1,9 @@
 package br.edu.fatecpg.deolhonolixo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class DeOlhoNoLixoApplication {
 
 	public static void main(String[] args) {
