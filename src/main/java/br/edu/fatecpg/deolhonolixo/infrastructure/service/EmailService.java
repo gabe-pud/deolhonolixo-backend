62a973a46f63cfdef4ce5435cff7352ba3495393
Thiago M. <185578934+thiishy@users.noreply.github.com>
2026-05-09 10:25:21 -0300

    feat: enhance EmailService with clean architeture good practices
    EmailService now implements EmailGateway, added subject parameter to sendEmail method and implemented Slf4j for logging

--- src/main/java/br/edu/fatecpg/deolhonolixo/infrastructure/service/EmailService.java
+++ src/main/java/br/edu/fatecpg/deolhonolixo/infrastructure/service/EmailService.java
@@ -2,7 +2,9 @@ package br.edu.fatecpg.deolhonolixo.infrastructure.service;
 
 import br.edu.fatecpg.deolhonolixo.core.domain.User;
 import br.edu.fatecpg.deolhonolixo.core.domain.exception.EmailNotSentException;
+import br.edu.fatecpg.deolhonolixo.core.gateway.EmailGateway;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -14,12 +16,13 @@ import org.thymeleaf.context.Context;
 import java.time.Year;
 import java.util.Map;
 
+@Slf4j
 @Service
-public class EmailService {
-    
+public class EmailService implements EmailGateway {
+
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
-    
+
     @Value("${spring.mail.username}")
     private String fromAddress;
 
@@ -34,15 +37,16 @@ public class EmailService {
         return templateEngine.process(templateName, context);
     }
 
+    @Override
     @Async
-    public void sendEmail(String templateName, Map<String, Object> templateVariables, User user) {
+    public void sendEmail(String templateName, String subject, Map<String, Object> templateVariables, User user) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
             helper.setFrom(fromAddress);
             helper.setTo(user.email());
-            helper.setSubject("De Olho no Lixo - Confirme o seu e-mail");
+            helper.setSubject(subject);
 
             templateVariables.put("currentYear", Year.now().getValue());
 
@@ -51,6 +55,7 @@ public class EmailService {
 
             mailSender.send(message);
         } catch (Exception e) {
+            log.error("Falha ao enviar e-mail: ", e);
             throw new EmailNotSentException();
         }
     }
