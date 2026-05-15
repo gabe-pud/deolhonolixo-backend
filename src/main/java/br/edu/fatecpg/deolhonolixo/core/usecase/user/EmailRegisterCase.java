7d8f8abae4891fffc4479a8f20d525c8ba3ea01c
Thiago M. <185578934+thiishy@users.noreply.github.com>
2026-05-15 18:29:52 -0300

    fix: add use case annotation in EmailRegisterCase

--- src/main/java/br/edu/fatecpg/deolhonolixo/core/usecase/user/EmailRegisterCase.java
+++ src/main/java/br/edu/fatecpg/deolhonolixo/core/usecase/user/EmailRegisterCase.java
@@ -2,9 +2,11 @@ package br.edu.fatecpg.deolhonolixo.core.usecase.user;
 
 import br.edu.fatecpg.deolhonolixo.core.domain.User;
 import br.edu.fatecpg.deolhonolixo.core.gateway.EmailGateway;
+import br.edu.fatecpg.deolhonolixo.infrastructure.config.annotations.UseCase;
 
 import java.util.Map;
 
+@UseCase
 public class EmailRegisterCase {
     private final EmailGateway emailGateway;
 
