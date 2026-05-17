60a0a80b1a0f1b06b5c2734487b9235c89d0bc82
Thiago M. <185578934+thiishy@users.noreply.github.com>
2026-05-17 18:07:27 -0300

    feat(cors): update CorsConfig to receive an environment variable

--- src/main/java/br/edu/fatecpg/deolhonolixo/infrastructure/config/cors/CorsConfig.java
+++ src/main/java/br/edu/fatecpg/deolhonolixo/infrastructure/config/cors/CorsConfig.java
@@ -1,16 +1,19 @@
 package br.edu.fatecpg.deolhonolixo.infrastructure.config.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    @Value("${deolhonolixo.cors.url}")
+    private String corsUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000")
+                .allowedOrigins(corsUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
